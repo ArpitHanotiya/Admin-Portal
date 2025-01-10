210aac01f6afdf2e8a39f751a5ceb1a72806569e
Arpit Hanotiya <arpithanotiya1@gmail.com>
2025-01-10 20:07:26 +0530

    connect database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
+const connectDb = require("./utils/db")
 
 app.use(express.json());
 
@@ -17,7 +19,11 @@ app.use("/api/auth", router);
 // });
 
 const PORT = 5000
-app.listen(PORT, ()=>{
-    console.log(`server is running at ${PORT}`);
+
+connectDb().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`server is running at ${PORT}`);
+    });
 });
+
 
