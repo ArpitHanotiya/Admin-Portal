4ea42107f9550051237b3166bce3c06dd733a0ec
Arpit Hanotiya <arpithanotiya1@gmail.com>
2025-01-11 14:40:26 +0530

    storing registered user data

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user-model")
+
 const home = async(req, res) => {
     try {
         res
@@ -11,9 +13,23 @@ const home = async(req, res) => {
 const register = async(req, res) => {
     try {
         // console.log(req.body);
+        // const data = req.body;
+        const {username, email, phone, password} = req.body;
+
+        // const userExist = User.findOne({email: email});
+        // field : value, when both are same can write it once
+        const userExist = await User.findOne({email});
+
+
+        if(userExist){
+            return res.status(400).json({msg : "email already exist"});
+        }
+
+        const userCreated = await User.create({username, email, phone, password});
+
         res
             .status(200)
-            .json({message: req.body});
+            .json({msg : userCreated});
     } catch (error) {
         res.status(500).json("internal server error");
     }
